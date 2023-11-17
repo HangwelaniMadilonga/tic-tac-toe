72e47e50051ccd180fd98cc12ad8fbf1f6bb9414
Hangwelani Madilonga <hangwelani3@gmail.com>
2023-11-17 11:20:40 +0200

    Add factory function for creating player.

---
+++ app.js
@@ -0,0 +1,9 @@
+function createPlayer (name) {
+    const Player = name;
+    const playerSymbol = "";
+    let playerScore = 0;
+    const getPoint = () => point;
+    const givePoint = () => point++;
+  
+    return { };
+  }

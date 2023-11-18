118dcaa504f258de9e2e57cb049aeb704318b7c5
Hangwelani Madilonga <hangwelani3@gmail.com>
2023-11-18 07:01:30 +0200

    update game logic.

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+function createBoard (firstName,secondName) {
+    let gameBoard=[["","",""],
+                   ["","",""],
+                   ["","",""]];
+    let firstPlayer = createPlayer(firstName);
+    let firstPlayerSymbol = "X";
+    let secondPlayer = createPlayer(secondName);
+    let secondPlayerSymbol = "O";
+  
+    return {};
+  }
+
 function createPlayer (name) {
     const Player = name;
-    const playerSymbol = "";
-    let playerScore = 0;
     const getPoint = () => point;
     const givePoint = () => point++;
   
-    return { };
+    return {Player,playerSymbol,playerScore};
   }

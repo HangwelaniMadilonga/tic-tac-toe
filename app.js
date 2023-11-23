7a58a61265ba936bcb349ab3fba30d76fb38e85c
Hangwelani Madilonga <hangwelani3@gmail.com>
2023-11-23 11:53:11 +0200

    Remove loop bug and append game stage.

--- app.js
+++ app.js
@@ -13,23 +13,21 @@ function createBoard() {
   let computerPlayer = createPlayer("O");
   let isFirstTurn = true;
   let isPlaying = true;
-  while (isPlaying) {
-    if (isFirstTurn) {
-      let screen = controlScreen(gameBoard, firstPlayer.playerSymbol);
-      screen.beginGame();
-    } else {
-      let screen = controlScreen(gameBoard, secondPlayer.playerSymbol);
-      een(gameBoard, firstPlayer.playerSymbol);
-    }
+
+  if (isFirstTurn) {
+    let screen = controlScreen(gameBoard, firstPlayer.playerSymbol);
+    screen.beginGame();
+    isFirstTurn = !isFirstTurn;
+  } else {
+    let screen = controlScreen(gameBoard, secondPlayer.playerSymbol);
+    isFirstTurn = !isFirstTurn;
   }
 }
 
 function createPlayer(playerSymbol) {
-  const playerSymbol = playerSymbol;
   let point = 0;
   const getPoint = () => point;
   const givePoint = () => point++;
-
   return { playerSymbol, getPoint, givePoint };
 }
 
@@ -39,11 +37,13 @@ let controlScreen = (gameBoard, playerSymbol) => {
     while (gameContainer.firstChild) {
       gameContainer.removeChild(gameContainer.firstChild);
     }
+
     for (let i = 0; i < gameBoard.length; i++) {
       for (let j = 0; j < gameBoard[i].length; j++) {
         const div = document.createElement("div");
         div.classList.add("cell");
         div.setAttribute("data-index", `${i}-${j}`);
+        gameContainer.appendChild(div);
         div.addEventListener("click", () => {
           const dataIndex = div.dataset.index;
           const [i, j] = dataIndex.split("-").map(Number);
@@ -67,9 +67,8 @@ let controlScreen = (gameBoard, playerSymbol) => {
 
   return { beginGame };
 };
-
-//what does the display screen do?
+playGame();
 /*
-  it returns functions for manipulating the screen.
-  we create the 
+  we cant use a number because it i
+
   sdf*/
